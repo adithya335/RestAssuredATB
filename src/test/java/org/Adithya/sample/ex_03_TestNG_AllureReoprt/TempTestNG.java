32fc2bad6b332f2aa385084679b8329d7979a09b
ADITHYA <sampathadithya11@gmail.com>
2025-08-11 11:47:15 +0530

    APi learning 6-11th aug

---
+++ src/test/java/org/Adithya/sample/ex_03_TestNG_AllureReoprt/TempTestNG.java
@@ -0,0 +1,11 @@
+package org.Adithya.sample.ex_03_TestNG_AllureReoprt;
+
+import org.testng.annotations.Test;
+
+public class TempTestNG {
+
+    @Test
+    public void test_pramod(){
+        System.out.println("Hello");
+    }
+}
